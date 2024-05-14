ef1549406e85420e38510e7185764ad081155c40
Andy Lin <andy.lin@elasticpath.com>
2024-05-14 13:28:10 +0100

    fix: user retrieval

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -3,17 +3,18 @@ import {sql} from '@vercel/postgres';
 
 export async function retrieveUser(userEmail: string) {
     const user = await sql`SELECT u.guid, u.email, u.name, SUM(o.subtotal) AS loyalty_value
-    FROM users u
-    INNER JOIN order o ON u.guid = o.userGuid
+    FROM "user" u
+    INNER JOIN "order" o ON u.guid = o."userGuid"
     WHERE u.email = ${userEmail}
-    GROUP BY u.guid;    
+    GROUP BY u.guid;
     `;
 
     return user.rows;
 }
 
 export async function createOrder() {
-    await sql`INSERT INTO order (guid, createdDate, userGuid, subtotal, shipping, total)
+    // WIP
+    await sql`INSERT INTO "order" (guid, createdDate, "userGuid", subtotal, shipping, total)
     VALUES ('ORD123', 'user123', 50.00, CURRENT_DATE());
     `;
 }
