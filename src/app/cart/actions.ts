8c5e832b3bc09a71975b1acfdfdbf34608baac86
Andy Lin <andy.lin@elasticpath.com>
2024-05-14 12:36:58 +0100

    feat: user retrieval query

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -1,8 +1,19 @@
 "use server"
 import {sql} from '@vercel/postgres';
 
-export async function exampleAction() {
-    const temp = await sql`SELECT * FROM temp`;
+export async function retrieveUser(userEmail: string) {
+    const user = await sql`SELECT u.guid, u.email, u.name, SUM(o.subtotal) AS loyalty_value
+    FROM users u
+    INNER JOIN order o ON u.guid = o.userGuid
+    WHERE u.email = ${userEmail}
+    GROUP BY u.guid;    
+    `;
 
-    return temp.rows;
+    return user.rows;
+}
+
+export async function createOrder() {
+    await sql`INSERT INTO order (guid, createdDate, userGuid, subtotal, shipping, total)
+    VALUES ('ORD123', 'user123', 50.00, CURRENT_DATE());
+    `;
 }
